21db0a1fefd3506741e60d5396cdba94fb1f99df
joe.phoenix <joe.phoenix@gmail.com>
2014-06-17 21:10:55 +0800

    修正第三十四个界面的内容，如图片的名称更改和水平翻折的效果。

--- src/phoenix/practise/fifthLowerPart/F34/ReverseApplet.java
+++ src/phoenix/practise/fifthLowerPart/F34/ReverseApplet.java
@@ -4,6 +4,7 @@ import java.applet.Applet;
 import java.awt.*;
 import java.net.URL;
 
+@SuppressWarnings("serial")
 public class ReverseApplet extends Applet implements Runnable {
 
 	// 图像数组及当前显示图像
@@ -46,7 +47,7 @@ public class ReverseApplet extends Applet implements Runnable {
 		// 得到延迟参数
 		String param = getParameter("delay");
 		if (param == null) {
-			delay = 3000;
+			delay = 1000;
 		} else {
 			delay = Integer.parseInt(param);
 		}
@@ -84,7 +85,7 @@ public class ReverseApplet extends Applet implements Runnable {
 			try {
 				// 实现图片的翻折效果
 				for (int i = 0; i <= (imageHeight / 2); i++) {
-					thread.sleep(30);
+					Thread.sleep(30);
 					graphics.setColor(Color.white);
 					graphics.fillRect(0, 0, imageWidth, imageHeight);
 
@@ -98,16 +99,19 @@ public class ReverseApplet extends Applet implements Runnable {
 				currentImage = ((currentImage + 1) % totalImage);
 
 				// 实现图片的反向翻折效果
-				for (int i = 0; i <= (imageHeight / 2); i++) {
-					thread.sleep(30);
+				for (int i = 0; i <= (imageWidth / 2); i++) {
+					Thread.sleep(30);
 					graphics.setColor(Color.white);
 					graphics.fillRect(0, 0, imageWidth, imageHeight);
-					graphics.drawImage(images[currentImage], 0,
-							(imageHeight / 2) - i, imageWidth, 2 * i, this);
+
+					// 添加水平翻折实现
+					graphics.drawImage(images[currentImage], (imageWidth / 2)
+							- i, 0, 2 * i, imageHeight, this);
 					repaint();
 				}
-				thread.sleep(delay);
+				Thread.sleep(delay);
 			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
