77bd5a96eeb787559da06f58309d596104a388f2
joephoenix <joe.phoenix@gmail.com>
2014-12-01 12:23:19 +0800

    添加了InsertionSort和ShellSort的方法，并且添加了相应的说明。

--- src/phoenix/DataStruct/SortTest.java
+++ src/phoenix/DataStruct/SortTest.java
@@ -5,14 +5,28 @@ import phoenix.DataStruct.Sort.Sort;
 public class SortTest {
 
 	public static void main(String[] args) {
-		Integer[] array = { 1, 3, 1, 4, 2, 4, 2, 3, 2, 4, 7, 6, 6, 7, 5, 5, 7,
-				7 };
+		Integer[] array1 = { 1, 3, 1, 4, 2, 4, 2, 3, 2, 4, 7, 6, 6, 7, 5, 5, 7,
+				7, 9, -1, -2, 4, 8 };
+		Integer[] array2 = { 1, 3, 1, 4, 2, 4, 2, 3, 2, 4, 7, 6, 6, 7, 5, 5, 7,
+				7, 9, -1, -2, 4, 8 };
+		Integer[] array3 = { 1, 3, 1, 4, 2, 4, 2, 3, 2, 4, 7, 6, 6, 7, 5, 5, 7,
+				7, 9, -1, -2, 4, 8 };
 		System.out.println("Before SelectionSort:");
-		printArray(array);
-		Sort<Integer> sortarray = new Sort<Integer>();
-		sortarray.selectSort(array);
+		printArray(array1);
+		Sort<Integer> sortA = new Sort<Integer>();
+		sortA.selectSort(array1);
 		System.out.println("After SelectionSort:");
-		printArray(array);
+		printArray(array1);
+		System.out.println("Before InsertionSort");
+		printArray(array2);
+		sortA.insertSort(array2);
+		System.out.println("After InsertionSort:");
+		printArray(array2);
+		System.out.println("Before ShellSort");
+		printArray(array3);
+		sortA.shellSort(array3);
+		System.out.println("After ShellSort:");
+		printArray(array3);
 	}
 
 	protected static void printArray(Integer[] array) {
